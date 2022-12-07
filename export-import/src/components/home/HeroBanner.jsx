9f56b5c9bc28725041c41eaa9ddda44e9705009b
AhianyoElisha <elisha.ahianyo247@gmail.com>
2022-12-07 13:36:07 +0000

    made necessary changes

--- export-import/src/components/home/HeroBanner.jsx
+++ export-import/src/components/home/HeroBanner.jsx
@@ -4,7 +4,7 @@ const HeroBanner = () => {
   return (
     <div className="home__data">
         <h4 className="home__title home-in">Quality and Performance</h4>
-        <h2 className="home__subtitle home-in">Manage Exports International</h2>
+        <h2 className="home__subtitle home-in">Mange Exports International</h2>
         <p className="home__description home-in">Vast experience with the best products on the market</p>
         <a href="#read__more" className="button button--flex home-in">READ MORE</a>
     </div>
